be53207724b7bb1ae565de377337e0b75d9b59fb
TAIJULAMAN <taijulaman8@gmail.com>
2023-09-02 22:44:54 +0600

    a easy responsive react news website

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUXvJK2XV11GkBvJEDg_jq0VADOc7H8N4",
+  authDomain: "responsive-react-web-8c8e9.firebaseapp.com",
+  projectId: "responsive-react-web-8c8e9",
+  storageBucket: "responsive-react-web-8c8e9.appspot.com",
+  messagingSenderId: "120128516828",
+  appId: "1:120128516828:web:9dad6e0d00a0837a40ba19"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
